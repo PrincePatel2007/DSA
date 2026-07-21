0a13ccbd01655ee8d264ebbd7ffc967605f9485c
Prince Patel <princempatel2007@gmail.com>
2026-07-21 17:31:27 +0530

    made console details user friendly

--- Lab/20260720/Exp1/matrix_mul_3x3.c
+++ Lab/20260720/Exp1/matrix_mul_3x3.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main() {
     int arr1[3][3], arr2[3][3], _mul[3][3];
+
+    printf("Enter the elements of first 3x3 matrix:\n");
     for (int i=0; i<3; i++) {
         printf("\n");
         for (int j=0; j<3; j++) {
@@ -8,6 +10,7 @@ int main() {
         }
     }
 
+    printf("Enter the elements of second 3x3 matrix:\n");
     for (int i=0; i<3; i++) {
         printf("\n");
         for (int j=0; j<3; j++) {
@@ -15,6 +18,7 @@ int main() {
         }
     }
     
+    printf("The product of the two matrices is:\n");
     for (int i=0; i<3; i++) {
         for (int j=0; j<3; j++) {
             _mul[i][j] = arr1[i][0]*arr2[0][j] + arr1[i][1]*arr2[1][j] + arr1[i][2]*arr2[2][j];
