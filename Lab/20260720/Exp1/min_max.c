ad5b06728233f82230d9214c1600374f342ec560
Prince Patel <princempatel2007@gmail.com>
2026-07-22 19:29:30 +0530

    checking remove function

--- Lab/20260720/Exp1/min_max.c
+++ Lab/20260720/Exp1/min_max.c
@@ -1,37 +1,47 @@
 #include<stdio.h>
 
+int len (int arr[]) {
+    int _length = sizeof(arr) / sizeof(arr[0]);
+    return _length;
+}
+
 int min(int arr[]) {
-    int len = sizeof(arr) / sizeof(arr[0]);
     int min = arr[0];
-    for (int i=0; i<len; i++) {
+    for (int i=0; i<len(arr); i++) {
         if (min<arr[i]) {
             min = arr[i];
         }
     }
-    return 0;
+    return min;
 }
 
 int max(int arr[]) {
-    int len = sizeof(arr) / sizeof(arr[0]);
     int max = arr[0];
-    for (int i=0; i<len; i++) {
+    for (int i=0; i<len(arr); i++) {
         if (max>arr[i]) {
             max = arr[i];
         }
     }
-    return 0;
+    return max;
 }
 
 int remove(int el, int arr[]) {
-    int len = sizeof(arr) / sizeof(arr[0]);
-    for (int i=0; i<len; i++) {
+    for (int i=0; i<len(arr); i++) {
         if (arr[i]==el) {
             for (int j=i; j<len-1; j++) {
                 arr[j] = arr[j+1];
             }
         }
     }
-    return 0;
+    return NULL;
+}
+
+int main() {
+    int arr[5] = {2,6,4,9,7};
+    remove(9, arr);
+    for (int i=0; i<len(arr); i++) {
+        printf("%d", arr[i]);
+    }
 }
 
 
