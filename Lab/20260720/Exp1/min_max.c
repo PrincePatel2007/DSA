d1026c0af29781ea5b068f524a696272372f9a9e
Prince Patel <princempatel2007@gmail.com>
2026-07-23 09:51:56 +0530

    testing remove function

--- Lab/20260720/Exp1/min_max.c
+++ Lab/20260720/Exp1/min_max.c
@@ -26,8 +26,20 @@ int _remove(int el, int arr[], int len) {
             for (int j=i; j<len-1; j++) {
                 arr[j] = arr[j+1];
             }
+            arr[len-1] = 0;
         }
     }
+    return 0;
+}
+
+int main() {
+    int arr[5] = {2, 8, 5, 9, 4};
+    int len = 5;
+    _remove(9, arr, len);
+    for (int i=0; i<len-1; i++) {
+        printf("%d   ", arr[i]);
+    }
+
     return 0;
 }
 
