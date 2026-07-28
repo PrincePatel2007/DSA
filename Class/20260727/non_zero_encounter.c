73a5b27a16f684cfa91324f217107bbb6af69a78
Prince Patel <princempatel2007@gmail.com>
2026-07-28 14:58:52 +0530

    completed code successfully

--- Class/20260727/non_zero_encounter.c
+++ Class/20260727/non_zero_encounter.c
@@ -10,19 +10,25 @@ int main () {
       }
     }
     int filtered_arr[non_zero_len][3];
-    int value, row, column count=0;
+    int count=0;
     for (int i=0; i<4; i++) {
       for (int j=0; j<5; j++) {
         if (arr[i][j]!=0) {
           filtered_arr[count][0] = arr[i][j];
           filtered_arr[count][1] = i;
           filtered_arr[count][2] = j;
+          count++;
         }
       }
     }
+
+    printf("Value  Row  Column");
     for (int i=0; i<non_zero_len; i++) {
+      printf("\n");
       for (int j=0; j<3; j++) {
-        
+        printf("  %d   ", filtered_arr[i][j]);
       }
     }
+
+    return 0;
 }
