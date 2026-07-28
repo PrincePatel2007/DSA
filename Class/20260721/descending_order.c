547fe575274eff6d4887ecb06e3672446abeeaf5
Prince Patel <princempatel2007@gmail.com>
2026-07-28 14:51:36 +0530

    successful code

--- Class/20260721/descending_order.c
+++ Class/20260721/descending_order.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 
 int main() {
-    int arr[] = {2,1,4,5,7};
+    int arr[5] = {2,1,4,5,7};
     int temp;
     for (int i=0; i<5; i++) {
-        
+        for (int j=i+1; j<5; j++) {
+            if (arr[i]<arr[j]) {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
     }
+    for (int k=0; k<5; k++) {
+        printf("%d ", arr[k]);
+    }
+    return 0;
 }
