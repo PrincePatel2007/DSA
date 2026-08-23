b7e0f17d75198699729e44ac18f40a657f54c479
Prince Patel <princempatel2007@gmail.com>
2026-08-23 18:32:47 +0530

    fixed code

--- Lab/20260727/Exp2/Array_pointers.c
+++ Lab/20260727/Exp2/Array_pointers.c
@@ -4,14 +4,14 @@ int main() {
     int *ptr = arr;
     printf("Pointer to Array\n%d\n", *ptr + 1);
     int a[5] = {6, 7, 8, 9, 10};
-    int arr1[5];
+    int *arr1[5];
     for(int i = 0; i < 5; i++) {
-        arr1[i] = a[i];
+        arr1[i] = &a[i];
     }
 
     printf("Array of Pointer\n");
     for(int i = 0; i < 5; i++) {
-        printf("%d ", arr1[i]);
+        printf("%d ", *arr1[i]);
     }
 
     return 0;
